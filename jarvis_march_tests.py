ed42d7428046a36a28353b3c13a300166afada6c
Jon Alligood <jalligood@moffattnichol.com>
2019-02-19 12:26:43 -0500

    Update jarvis_march to use a ConvexHull object

--- jarvis_march_tests.py
+++ jarvis_march_tests.py
@@ -2,7 +2,7 @@ import unittest
 from unittest import TestCase
 
 from geometry import Point
-from jarvis_march import jarvis
+from jarvis_march import ConvexHull
 
 
 class JarvisMarchTest(TestCase):
@@ -16,14 +16,14 @@ class JarvisMarchTest(TestCase):
         ]
 
     def test_jarvis_march(self):
-        hull = jarvis(self.point_list)
+        hull = ConvexHull(self.point_list)
         expected_hull = [
             Point(0, 0),
             Point(0, 2),
             Point(2, 2),
             Point(2, 0)
         ]
-        self.assertEqual(hull, expected_hull)
+        self.assertEqual(hull.hull_points, expected_hull)
 
 if __name__ == '__main__':
     unittest.main()
